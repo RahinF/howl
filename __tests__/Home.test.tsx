c4d8ec49812c13a90fa46d2289db5209f1aa45b8
RahinF <s3297352@student.rmit.edu.au>
2023-10-03 19:43:20 +1100

    fixed test

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -2,11 +2,10 @@ import Home from '@/app/page';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 
-describe('Customer Reviews', () => {
+describe('Home', () => {
   it('renders heading', () => {
     render(<Home />);
-    const heading = screen.getByRole('heading', { level: 1 });
+    const heading = screen.getByRole('heading', { level: 1, name: /home/i });
     expect(heading).toBeInTheDocument();
-    expect(heading).toHaveTextContent(/home/i);
   });
 });
